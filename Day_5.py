db03f3aba61d34134a610ae9f8ab7b0543be2ebb
Javier Elohim Burgos Chaguezac <84932679+JavierBurgos-web@users.noreply.github.com>
2023-01-05 18:40:16 -0500

    Day_5.py upload

---
+++ Day_5.py
@@ -0,0 +1,10 @@
+from IPython.display import YouTubeVideo
+# importamos la libreria de YouTube para mostrar un video embedido en la ventana.
+
+YouTubeVideo('jej8qlzlAGw',width=800,height=400)
+#Notesé que solo se debe registrar el codigo del video, no la url completa.
+#Podemos especificar el tamaño del video siempre y cuando comprenda la relación aspecto.
+
+
+%who
+# Con Who detallamos todas las variables disponibles actualmente
